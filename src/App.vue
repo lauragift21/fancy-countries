b5e410e97c4a05269d4f0ee0cb4c046a586fd8a3
lauragift21 <laurandidi21@gmail.com>
2019-07-01 18:39:06 +0100

    Add vue to project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <AppNav />
+  </div>
+</template>
+
+<script>
+import AppNav from "./components/AppNav.vue";
+
+export default {
+  name: "app",
+  components: {
+    AppNav
+  }
+};
+</script>
+
+<style>
+#app {
+  max-width: 1300px;
+  margin: 0 auto;
+  font-family: 'Nunito Sans', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #48494b;
+}
+</style>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>Where in the world?</li>
+        <li>
+          <img src="../assets/moon-regular.svg" width="20" />
+          Dark Mode
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+nav {
+  ul {
+    list-style: none;
+    li:first-child {
+      float: left;
+      font-size: 24px;
+      font-weight: 700;
+    }
+    li:last-child {
+      float: right;
+    }
+  }
+}
+</style>
